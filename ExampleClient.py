4038c0bb66baa8f050a8c5714ee45521b821396e
Alex L <edl010@ucsd.edu>
2024-04-03 13:38:37 -0700

    modifies example client

--- ExampleClient.py
+++ ExampleClient.py
@@ -21,7 +21,6 @@ from paho import mqtt
 
 
 
-
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
     """
@@ -89,9 +88,9 @@ client.on_connect = on_connect
 # enable TLS for secure connection
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 # set username and password
-client.username_pw_set("{YOUR USERNAME}", "{YOUR PASSWORD}")
+client.username_pw_set("Test1Test2", "Test12**")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("{YOUR URL}", 8883)
+client.connect("290fb679585f49149b9e3abbb69d7a55.s1.eu.hivemq.cloud", 8883)
 
 
 # setting callbacks, use separate functions like above for better visibility
@@ -101,12 +100,16 @@ client.on_publish = on_publish
 
 
 # subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("encyclopedia/#", qos=1)
+client.subscribe("#", qos=1)
 
 
-# a single publish, this can also be done in loops, etc.
-client.publish("encyclopedia/temperature", payload="hot", qos=1)
+# # a single publish, this can also be done in loops, etc.
+# client.publish("encyclopedia/temperature", payload="hot", qos=1)
 
+message = input("Enter message:")
+topic = input("Enter topic:")
+
+client.publish("%s" % (topic), payload="%s" % (message), qos=1)
 
 # loop_forever for simplicity, here you need to stop the loop manually
 # you can also use loop_start and loop_stop
